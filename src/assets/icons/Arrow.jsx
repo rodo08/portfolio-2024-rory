564d33e75e58f99d2f6b7d1021637989a3eff9e0
rodo08 <rodrigo.rosalesmoya@gmail.com>
2024-07-04 14:24:35 +0200

    Fix general styles at contact and design works view

---
+++ src/assets/icons/Arrow.jsx
@@ -0,0 +1,16 @@
+export function Arrow(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1rem"
+      height="1rem"
+      viewBox="0 0 2048 2048"
+      {...props}
+    >
+      <path
+        fill="#ffa800"
+        d="M1024 0h1024v1024h-128V219L93 2045l-90-90L1829 128h-805z"
+      ></path>
+    </svg>
+  );
+}
--- src/components/brandCard/BrandCard.jsx
+++ src/components/brandCard/BrandCard.jsx
@@ -1,9 +1,11 @@
+import { Arrow } from "../../assets/icons/Arrow";
 import "../projectCard/ProjectCard.css";
 
 const DesignSampleCard = ({ img, alt, width, height, onGoTo }) => {
   return (
-    <li className="project-card">
+    <li className="project-card" style={{ position: "relative" }}>
       <img src={img} height={width} width={height} alt={alt} onClick={onGoTo} />
+      <Arrow />
     </li>
   );
 };
